4e86eccc60fbc47a6510ac0df5bd653c5116738b
balibabu <balibabuchauhan1@gmail.com>
2026-05-18 18:27:50 +0545

    tested on linux, made compact

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
