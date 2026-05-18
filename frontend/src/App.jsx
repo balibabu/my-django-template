73acdd63d0210291c48734d4f63e6db7a78b00fc
balibabu <balibabuchauhan1@gmail.com>
2026-05-18 18:41:37 +0545

    added tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
     return (
         <div>
-            <h1>Hello world</h1>
+            <h1 className="text-xl font-bold text-blue-500">Hello world</h1>
         </div>
     );
 }
